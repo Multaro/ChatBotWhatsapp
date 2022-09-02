71e9bcd8f43ac247f91003386f193f03745e301c
Yago <yagocassiano@hotmail.com>
2022-09-02 20:54:31 -0300

    'update'

--- main.py
+++ main.py
@@ -2,27 +2,53 @@ import time
 
 from selenium import webdriver
 from simon.accounts.pages import LoginPage
+from simon.chat.pages import ChatPage
+from simon.chats.pages import PanePage
 from simon.header.pages import HeaderPage
-from simon.pages import BasePage
 
+
+# Creating the driver (browser)
 driver = webdriver.Firefox()
 driver.maximize_window()
 
+# Login
+#       and uncheck the remember check box
+#       (Get your phone ready to read the QR code)
 login_page = LoginPage(driver)
 login_page.load()
-login_page.remember_me = False
-time.sleep(7)
+print('tempo')
+time.sleep(20)
+print('fim')
 
-base_page = BasePage(driver)
-base_page.is_title_matches()
-base_page.is_welcome_page_available()
-base_page.is_nav_bar_page_available()
-base_page.is_search_page_available()
-base_page.is_pane_page_available()
+# 1. Get all opened chats
+#       opened chats are the one chats or conversations
+#       you already have in your whatsapp.
+#       IT WONT work if you are looking for a contact
+#       you have never started a conversation.
+pane_page = PanePage(driver)
+print(pane_page)
 
-base_page.is_chat_page_available()
+# get all chats
+print('pegando chats')
+opened_chats = pane_page.opened_chats
 
+print(opened_chats)
+
+for oc in opened_chats:
+    print(oc.name)  # contact name (as appears on your whatsapp)
+    print(oc.icon)  # the url of the image
+    print(oc.last_message)
+    print(oc.last_message_time)  # datetime object
+    print(oc.has_notifications())  # are there unread messages?
+    print(oc.notifications)  # returns a integer with the qty of new
+
+first_chat = opened_chats[0]
+first_chat.click()
+
+
+# Logout
 header_page = HeaderPage(driver)
 header_page.logout()
 
+# Close the browser
 driver.quit()
